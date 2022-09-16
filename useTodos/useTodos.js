f023cbde9dbcbad426dda2b365d7705d72446a04
Yovanny Zeballos <yovanny.zeballos@gmail.com>
2022-09-15 22:49:11 -0500

    todoReducer agregado

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from "react";
-import { todoReducer } from "../08-useReduce/todoReducer";
+import { todoReducer } from "./todoReducer";
 
 export const useTodos = () => {
 
